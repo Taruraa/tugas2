bd66782b75e821fbe39001daf256ba5e1f209f86
Prian Widadul Halim <129753069+Taruraa@users.noreply.github.com>
2023-10-23 17:37:52 +0700

    Update tugas.php

--- tugas.php
+++ tugas.php
@@ -4,11 +4,17 @@
     </head>
     <body>
         <form action="tugasLanjutan.php" method="post">
-            <h2>menggunakan program perkalian</h2>
-            Masukkan nilai i : <br>
-            <input type="number" name="pertama" placeholder="masukkan nilai..." required><br>
-            Masukkan nilai x : <br>
-            <input type="number" name="kedua" placeholder="masukkan nilai..." required><br><br>
+            <h2>Menggunakan Program Perkalian</h2>
+            <div>
+                <label for="pertama">Masukkan Nilai i</label>
+                <br>
+                <input type="number" name="pertama" placeholder="masukkan nilai..." required><br>
+            </div>
+            <div>
+                <label for="kedua">Masukkan Nilai x</label>
+                <br>
+                <input type="number" name="kedua" placeholder="masukkan nilai..." required><br><br>
+            </div>
             <button type="submit">kalikan</button>
         </form>
     </body>
